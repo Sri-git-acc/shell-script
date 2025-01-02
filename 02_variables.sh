a225bfa3051df6cecbdb4480cf31a1632462e6a9
sri-git-acc <sriy0697@gmail.com>
2025-01-02 19:48:44 +0530

    testing args

--- 02_variables.sh
+++ 02_variables.sh
@@ -3,11 +3,12 @@
 PER1=$1
 PER2=$2
 PER3=$3
+SEN=$4
 
 echo "$PER1: Hello $PER2"
 echo "$PER3: Hello $PER1, How are you?"
 echo "$PER1: I'm fine, How are you?"
 echo "$PER3: I'm good, I have something to say"
 echo "$PER1: Oh!, What is it?"
-echo "$PER3: We are live and scripting online"
-echo "$PER1: Oh!, Great"
+echo "$PER3: $SEN"
+echo "$PER1: :)"
