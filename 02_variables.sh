317fa16a3db7efcc275393ad3ac2ba5e01251197
sri-git-acc <sriy0697@gmail.com>
2025-01-02 20:08:41 +0530

    testing args

--- 02_variables.sh
+++ 02_variables.sh
@@ -3,15 +3,11 @@
 PER1=$1
 PER2=$2
 PER3=$3
-SEN1=$4
-SEN2=$5
-SEN3=$6
 
 echo "$PER1: Hello $PER2"
 echo "$PER3: Hello $PER1, How are you?"
 echo "$PER1: I'm fine, How are you?"
 echo "$PER3: I'm good."
-echo "$PER1: I have something to say"
-echo "$PER3: Oh!, What is it?"
-echo "$PER1: $SEN1 $SEN2 $SEN3"
-echo "$PER3: :)"
+echo "$PER1: Please enter your username: $USERNAME"
+read USERNAME
+echo "$PER1: Your username is: $USERNAME"
