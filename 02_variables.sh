a76c0e9d6fdae9781d2eca854a521195519303ae
sri-git-acc <sriy0697@gmail.com>
2025-01-02 19:35:17 +0530

    conversational script

---
+++ 02_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PER1=Sri
+PER2=A
+PER3=ANUDEEP
+
+echo "$PER1: Hello $PER2"
+echo "$PER3: Hello $PER2, How are you?"
+echo "$PER1: I'm fine, How are you?"
+echo "$PER3: I'm good and love you"
