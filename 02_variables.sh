c6b77674d681b9d1b841793835f0919de5a231a5
sri-git-acc <sriy0697@gmail.com>
2025-01-02 19:42:04 +0530

    added few more lines

--- 02_variables.sh
+++ 02_variables.sh
@@ -5,6 +5,9 @@ PER2=A
 PER3=ANUDEEP
 
 echo "$PER1: Hello $PER2"
-echo "$PER3: Hello $PER2, How are you?"
+echo "$PER3: Hello $PER1, How are you?"
 echo "$PER1: I'm fine, How are you?"
-echo "$PER3: I'm good and love you"
+echo "$PER3: I'm good, I have something to say"
+echo "$PER3: Oh!, What is it?"
+echo "$PER1: We are live and scripting online"
+echo "$PER3: Oh!, Great"
