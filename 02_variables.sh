7a4f64b67070e8db9c871ba8aebd171fcc7e9e34
sri-git-acc <sriy0697@gmail.com>
2025-01-02 20:11:11 +0530

    testing args

--- 02_variables.sh
+++ 02_variables.sh
@@ -9,5 +9,5 @@ echo "$PER3: Hello $PER1, How are you?"
 echo "$PER1: I'm fine, How are you?"
 echo "$PER3: I'm good."
 echo "$PER1: Please enter your username:$USER"
-read USER
+read -s USER
 echo "$PER1: Your username is: $USER"
