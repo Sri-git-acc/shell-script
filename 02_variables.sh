c91c560cafc95e4ae7cfa21ab8003a896531c056
sri-git-acc <sriy0697@gmail.com>
2025-01-03 12:25:27 +0530

    testing variables

--- 02_variables.sh
+++ 02_variables.sh
@@ -9,5 +9,5 @@ echo "$PER3: Hello $PER1, How are you?"
 echo "$PER1: I'm fine, How are you?"
 echo "$PER3: I'm good."
 echo "$PER1: Please enter your username:"
-read -s USER
+read -s USER # read -s is to hide the content we are inputting
 echo "$PER1: Your username is: $USER"
--- 1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello World!"
-echo "test"
-echo "test2"
-echo "test3"
