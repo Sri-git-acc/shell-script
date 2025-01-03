12cc3721015fb80cbb0161b4ec09c40a4c12c091
sri-git-acc <sriy0697@gmail.com>
2025-01-03 12:41:14 +0530

    testing conditions

--- 06_conditions.sh
+++ 06_conditions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Enter your number:"
-read NUM
+#echo "Enter your number:"
+#read NUM
+NUM=$1
 
 if [ $NUM -gt 100]
 then
-    echo "Given number is greater tha 100"
+    echo "Given number is greater than 100"
 else
     echo "Given number is less than 100"
