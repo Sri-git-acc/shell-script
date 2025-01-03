898c7d1ed6ce95ab094fee9760c5b13ee0502934
sri-git-acc <sriy0697@gmail.com>
2025-01-03 15:54:14 +0530

    installing git in server

--- 07_git_installation.sh
+++ 07_git_installation.sh
@@ -10,34 +10,57 @@ then
     exit 1
 fi
 
-dnf list installed git
-if [ $? -eq 0 ]
-then
-    echo "git already installed and up-to-date"
-else
-    dnf install git -y
+VALIDATE(){
 
-    if [ $? -ne 0 ]
-    then   
-        echo "git installation failed"
-        exit 1
-    else   
-        echo "git installation is success"
+    dnf list installed $1
+    if [ $2 -eq 0 ]
+    then
+        echo "$1 already installed and up-to-date"
+    else
+        dnf install $1 -y
+
+        if [ $1 -ne 0 ]
+        then   
+            echo "$1 installation failed"
+            exit 1
+        else   
+            echo "$1 installation is success"
+        fi
     fi
-fi
+}
+
+VALIDATE() "git" $? 
+VALIDATE() "ansible" $?
+
+# dnf list installed git
+# if [ $? -eq 0 ]
+# then
+#     echo "git already installed and up-to-date"
+# else
+#     dnf install git -y
+
+#     if [ $? -ne 0 ]
+#     then   
+#         echo "git installation failed"
+#         exit 1
+#     else   
+#         echo "git installation is success"
+#     fi
+# fi
 
 
-dnf list installed git
-if [ $? -eq 0 ]
-then
-    echo "ansible already installed and up-to-date"
-else
-    dnf install ansible -y
-    if [ $? -ne 0 ]
-    then   
-        echo "ansible installation failed"
-        exit 1
-    else   
-        echo "ansible installation is success"
-    fi
-fi
+# dnf list installed ansible
+# if [ $? -eq 0 ]
+# then
+#     echo "ansible already installed and up-to-date"
+# else
+#     dnf install ansible -y
+#     if [ $? -ne 0 ]
+#     then   
+#         echo "ansible installation failed"
+#         exit 1
+#     else   
+#         echo "ansible installation is success"
+#     fi
+# fi
+
