8fbbe8dc6d1b76d5cb7db25c52e2413d88c2510e
sri-git-acc <sriy0697@gmail.com>
2025-01-03 13:39:48 +0530

    installing git in server

--- 07_git_installation.sh
+++ 07_git_installation.sh
@@ -7,6 +7,25 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then   
     echo "ERROR: Please change the user mode to root"
+    exit 1
 fi
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then   
+    echo "git installation failed"
+    exit 1
+else   
+    echo "git installation is success"
+fi
+
+dnf install ansible -y
+
+if [ $? -ne 0 ]
+then   
+    echo "ansible installation failed"
+    exit 1
+else   
+    echo "ansible installation is success"
+fi
