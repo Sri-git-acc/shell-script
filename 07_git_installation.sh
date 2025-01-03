f4bdd1b60fe38288dd744e72631d902bc2be8be2
sri-git-acc <sriy0697@gmail.com>
2025-01-03 13:29:23 +0530

    installing git in server

--- 07_git_installation.sh
+++ 07_git_installation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-USER=$USER
+#USER=$USER
+USER=$(id -u)
 
-if [ $USER -ne root]
+#if [ $USER -ne root ]
+if [ $USER -ne 0 ]
 then   
     echo "ERROR: Please change the user mode to root"
 fi
