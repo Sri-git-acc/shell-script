b780f262568495c4669e5e84f8730669b6684ce5
sri-git-acc <sriy0697@gmail.com>
2025-01-04 14:18:56 +0530

    testing loops

--- 08_loops.sh
+++ 08_loops.sh
@@ -1,6 +1,56 @@
 #!/bin/bash
 
-for i in {0..6}
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+#if [ $USER -ne root ]
+if [ $USER -ne 0 ]
+then   
+    echo -e "$R ERROR: Please change the user mode to root $N"
+    exit 1
+fi
+
+VALIDATE(){
+
+    dnf list installed $1 &>>$LOG_FILE_NAME
+    if [ $? -eq 0 ]
+    then 
+        echo -e "$1 already installed and $Y up-to-date $N"
+    else
+        dnf install $1 -y &>>$LOG_FILE_NAME
+
+        if [ $1 -ne 0 ]
+        then   
+            echo -e "$1 installation $R failed $N"
+            exit 1
+        else   
+            echo -e "$1 installation is $G success $N" 
+        fi
+    fi
+}
+
+for package in $@
 do
-    echo $i
+    VALIDATE package
 done
+
+
+# VALIDATE "python" 
+# VALIDATE "ansible"
+
+
+#///////////////////////////////////////////////////////*Phase-1*//////////////////////////////////////////////////
+
+#!/bin/bash
+
+# for i in {0..6}
+# do
+#     echo $i
+# done
