7198ff805ce659887e7d13c6cbcf05defd266a78
sri-git-acc <sriy0697@gmail.com>
2025-01-04 22:05:54 +0530

    testing older file deletion process

--- 09_delete.sh
+++ 09_delete.sh
@@ -2,5 +2,5 @@
 
 SOURCE_DIR="/home/ec2-user"
 
-FILES_TO_DELETE=&(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "File/Files to delete: $FILES_TO_DELETE"
+FILES_TO_DELETE=&(find $SOURCE_DIR -name "*.logi" -mtime +14)
+echo "Files to delete: $FILES_TO_DELETE"
