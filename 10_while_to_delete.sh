dab1ea72ef4a04d317849163f5abca0f1736953c
sri-git-acc <sriy0697@gmail.com>
2025-01-04 22:21:44 +0530

    testing older file deletion process

--- 10_while_to_delete.sh
+++ 10_while_to_delete.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while read -r file <<< 09_delete.sh
+while read -r file 
 do
     echo "Deleting file: $file"
     rm -rf $file
     echo "Deleted file: $file"
-done 
+done < 09_delete.sh
