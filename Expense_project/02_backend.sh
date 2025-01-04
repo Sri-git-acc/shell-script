8bb9abc6640d1c02bc6435804d0ca5039192c14c
sri-git-acc <sriy0697@gmail.com>
2025-01-04 19:08:55 +0530

    settingup backend

---
+++ Expense_project/02_backend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+#if [ $USER -ne root ]
+if [ $USER -ne 0 ]
+then   
+    echo -e "$R ERROR: Please change the user mode to root $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+        then   
+            echo -e "$2 $R failed $N"
+            exit 1
+        else   
+            echo -e "$2 $G succeded $N" 
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "enablinging nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "installing nodejs"
+
+EXISTANCE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "$2 already exist $Y SKIPPING $N"
+    else    
+        echo "creating $2"
+    fi
+}
+
+useradd expense &>>$LOG_FILE_NAME
+EXISTANCE $? "expense username"
+
+mkdir /app &>>$LOG_FILE_NAME
+EXISTANCE $? "/app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+EXISTANCE $? "backend zip file"
+
+cd /app
+rm -rf /app/*
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+EXISTANCE $? "backend.zip"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "npm installation"
+
+cp /home/ec2-user/shell-script/Expense_project/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE_NAME
+VALIDATE $? "backend.servicefile transfer"
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "mysql installation"
+
+mysql -h <database.sriyaandansh.fun> -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
