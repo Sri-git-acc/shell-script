1f91ee48361f1559f0b5bf67504c6ebae919195c
sri-git-acc <sriy0697@gmail.com>
2025-01-05 12:25:56 +0530

    testing count and list of words

--- 10_count_and_list_words.sh
+++ 10_count_and_list_words.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-echo "Please enter your file name:"
-read -r FILENAME
-echo "Filename = $FILENAME"
+# echo "Please enter your file name:"
+# read -r FILENAME
+# echo "Filename = $FILENAME"
 
-COUNT=0
+# COUNT=0
 
-while read -r line;
-do
-    for word in $line;
-    do
-        COUNT++
-        echo "$word"
-    done
-    echo "Number of wordsare: $COUNT"
-done <<< $FILENAME
+# while read -r line;
+# do
+#     for word in $line;
+#     do
+#         COUNT++
+#         echo "$word"
+#     done
+#     echo "Number of words are: $COUNT"
+# done <<< $FILENAME
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+filename="$1"
+
+# Read the file word by word
+while read -r line; do
+  for word in $line; do
+    echo "$word"
+  done
+done < "$filename"
 
