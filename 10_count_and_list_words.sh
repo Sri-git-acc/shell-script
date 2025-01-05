bd892a0d6213010e3bea031a12e18208928a7a06
sri-git-acc <sriy0697@gmail.com>
2025-01-05 15:53:43 +0530

    testing count and list of words

--- 10_count_and_list_words.sh
+++ 10_count_and_list_words.sh
@@ -10,11 +10,14 @@ echo -e "Total word count of $FILE is: $COUNT"
 
 LIN=()
 
-while read -r line;
-do
-    for word in $line;
+VALIDATE() {
+    while read -r line;
     do
-        echo "$word"
-        uniq -c $word
-    done    
-done < $FILENAME
+        for word in $line;
+        do
+            echo "$word"
+        done    
+    done < $FILENAME
+}
+
+uniq -c < $VALIDATE
