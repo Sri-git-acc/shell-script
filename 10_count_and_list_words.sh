10785cf68214ee89b46cbfdc817d0a9c324a6ef6
sri-git-acc <sriy0697@gmail.com>
2025-01-05 15:03:27 +0530

    testing count and list of words

--- 10_count_and_list_words.sh
+++ 10_count_and_list_words.sh
@@ -5,15 +5,15 @@ read -r FILE
 FILEPATH="/home/ec2-user"
 FILENAME=$FILEPATH/$FILE
 echo "Filename = $FILE"
-COUNT= $(wc -w $FILENAME)
+COUNT=$(wc -w $FILENAME)
 echo -e "Total word count of $FILE is: $COUNT"
 
-# while read -r line;
-# do
-#     for word in $line;
-#     do
-#         echo "$word"
-#     done
-# done <<< $FILENAME
+while read -r line;
+do
+    for word in $line;
+    do
+        echo "$word"
+    done
+done <<< $FILENAME
 
 
