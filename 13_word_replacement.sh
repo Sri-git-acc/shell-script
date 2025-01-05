4e598f1d76fb6e4ed9657a0731ff0baf195a6a3e
sri-git-acc <sriy0697@gmail.com>
2025-01-05 18:21:43 +0530

    replacing words in a file

---
+++ 13_word_replacement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Please enter the file path to edit a word:"
+read -r FILE_PATH
+
+echo "Enter the word to replace:"
+read -r WORD
+
+echo "Enter the word to replace with:"
+read -r REP
+
+while read -r line
+do 
+    for word in $line
+    do
+        if [ word = $WORD ]
+        then
+            sed -i "s|$WORD|$REP|g"
+        fi
+    done
+done < $FILE_PATH
