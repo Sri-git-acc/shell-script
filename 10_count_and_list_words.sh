c0670395d2bc10cfee41b7b716f899eab091902b
sri-git-acc <sriy0697@gmail.com>
2025-01-05 12:18:17 +0530

    testing count and list of words

--- 10_count_and_list_words.sh
+++ 10_count_and_list_words.sh
@@ -4,12 +4,13 @@ echo "Please enter your file name:"
 read -r FILENAME
 echo "Filename = $FILENAME"
 
+COUNT=0
+
 while read -r line
 do
     for word in $line
-    COUNT=0
     do
-        COUNT+COUNT+1
+        COUNT=COUNT+1
         echo "$word"
     done
 done < $FILENAME
