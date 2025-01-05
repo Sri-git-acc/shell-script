98e40a48c5989d444cd504ffbd16723558694792
sri-git-acc <sriy0697@gmail.com>
2025-01-05 16:23:04 +0530

    renaming files with date

--- 11_file_rename.sh
+++ 11_file_rename.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 PATH="/home/ec2-user"
-FILES=$(find $PATH -name "*.log")
-echo "files to rename: $FILES"
+FILE_NAME=$(find $PATH -name "*.log")
+echo "files to rename: $FILE_NAME"
 TIMESTAMP=$(date +%Y-%m-%d)
 
 while read -r file
 do  
     mv $($PATH/$file) $($PATH/$TIMESTAMP_$file)
-done < $FILES
+done < $FILE_NAME
 
 
