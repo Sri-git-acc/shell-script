b050fa74edca9f92f5d1f181f0c20a46c777d440
sri-git-acc <sriy0697@gmail.com>
2025-01-05 20:59:39 +0530

    reversing content in a file line by line

--- 16_reverse_content.sh
+++ 16_reverse_content.sh
@@ -3,7 +3,8 @@
 echo "Please enter the file path:"
 read -r FILE_PATH
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+FILE=$(echo FILE_PATH | cut -d "/" -f4)
 
 REVER=$(cat $FILE_PATH | rev)
-touch /etc/$TIMESTAMP-reverse-$FILE_PATH
-mv $REVER /etc/$TIMESTAMP-reverse-$FILE_PATH
+touch /etc/$TIMESTAMP-reverse-$FILE
+mv $REVER /etc/$TIMESTAMP-reverse-$FILE
