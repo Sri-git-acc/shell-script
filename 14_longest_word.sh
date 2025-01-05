af128b26deaa212b4817faf66b9d3df68cd4ede0
sri-git-acc <sriy0697@gmail.com>
2025-01-05 18:57:01 +0530

    finding longest word in a file

---
+++ 14_longest_word.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Please enter the file path:"
+read -r FILE_PATH
+
+while read -r line
+do
+    for word in $line
+    do
+        F1=(wc -c $word)
+        echo "Word count of $word is: $F1"
+    done
+done
