fcfbcb7b4f1104363aadafb967c6936d18ffdfa3
sri-git-acc <sriy0697@gmail.com>
2025-01-05 16:56:18 +0530

    renaming files with date

--- 11_file_rename.sh
+++ 11_file_rename.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCE_DIR="/home/ec2-user"
+SOURCE_DIR="/home/ec2-user/shell-script"
 FILE_NAME=$(find $SOURCE_DIR -name "*.log")
 # echo "files to rename: $FILE_NAME"
 TIMESTAMP=$(date +%Y-%m-%d)
@@ -9,7 +9,7 @@ while read -r file
 do  
     cd /home/ec2-user
     echo "renaming file: $file"
-    F1=$($file | cut -d "/" -f4)
+    F1=$($file | cut -d "/" -f5)
     F2=$($TIMESTAMP_$F1)
     mv $F1 $F2
 done <<< $FILE_NAME
