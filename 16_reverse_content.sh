15de37f93dd99fc660c758ae6378a0ecf05010f0
sri-git-acc <sriy0697@gmail.com>
2025-01-06 08:26:53 +0530

    reversing content in a file and moving to another file

--- 16_reverse_content.sh
+++ 16_reverse_content.sh
@@ -5,10 +5,10 @@ read -r FILE_PATH
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 FILE=$(echo $FILE_PATH | cut -d "/" -f4)
 
-REVER=$(cat $FILE_PATH | rev)
 mkdir -p /home/ec2-user/reverse
-touch /home/ec2-user/reverse/$FILE
-cat $REVER > /home/ec2-user/reverse/$FILE
+REVER=$(cat $FILE_PATH | rev) &>>/home/ec2-user/reverse/$FILE
+
+
 
 
 
