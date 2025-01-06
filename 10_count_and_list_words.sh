9b8dd744886c7b5ea9499c504b666022cd6b6160
sri-git-acc <sriy0697@gmail.com>
2025-01-06 11:40:30 +0530

    print word occurance and their count

--- 10_count_and_list_words.sh
+++ 10_count_and_list_words.sh
@@ -5,17 +5,17 @@ read -r FILE_PATH
 
 # echo "Filename = $FILE_PATH"
 # COUNT=$(wc -w < $FILE_PATH)
-COUNT=$(uniq -w < $FILE_PATH)
+COUNT=$(wc -w < $FILE_PATH)
 echo -e "Total word count of $FILE_PATH is: $COUNT"
 
-# while read -r line;
-# do
-#     # sort $line | uniq -c
-#     for word in $line;
-#     do
-#         # echo "$word"
-#         sort $word | uniq -c
-#     done    
-# done < $FILE_PATH
+while read -r line;
+do
+    # sort $line | uniq -c
+    for word in $line;
+    do
+        # echo "$word"
+        sort | uniq -c
+    done    
+done < $FILE_PATH
 
 # ////////////////////////////////////////////////////////PENDING//////////////////////////////////////////////////////
