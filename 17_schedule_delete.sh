d18a09edc57a4b9479d916672801c6e606f2cb81
sri-git-acc <sriy0697@gmail.com>
2025-01-08 13:31:54 +0530

    scheduling auto deletion

--- 17_schedule_delete.sh
+++ 17_schedule_delete.sh
@@ -34,8 +34,20 @@ FILES=$(find $SOURCE_PATH -name "*.log" -mtime +$FILE_AGE)
 if [ -n "$FILES" ]
 then
     echo "Deleting file or files are: $FILES"
+    ZIP_FILE=$($DEST_PATH/$LOG_FILE-$TIMESTAMP.zip)
+    find $SOURCE_PATH -name "*.log" -mtime +$FILE_AGE | zip #@ "ZIP_FILE"
+    if [ -f $ZIP_FILE ]
+    then
+        echo "successfully created zip file for greater than 14days files"
+        while read -r dfile
+        do
+            rm -rf $dfile
+            echo "Deleted $dfile"
+        done < $FILES
+    else 
+        echo "Unable to zip file"
+else
+    echo "No file/files greater than $FILE_AGE days are found"
 fi
-
-echo $LOG_FILE
 
 
